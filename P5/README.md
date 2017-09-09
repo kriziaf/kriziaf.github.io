ba92116b34088f48da544c2816e0151d57de9154
kriziafern <kriziafernando@gmail.com>
2017-09-09 01:18:15 -0400

    added p5 Folder

---
+++ P5/README.md
@@ -0,0 +1,4 @@
+# P5 WEEK 1 Homework
+
+My attempt to design a nine dot test using p5 
+(the origin of the phrase "Out of the box")
---
+++ P5/sketch.js
@@ -0,0 +1,64 @@
+function setup() {
+    
+    createCanvas(400,400);
+    background(0, 0, 255);
+    
+}
+
+function draw() {
+    line(50, 95, 750, 300)
+    
+    line(50, 95, 750, 500)
+    
+    line(50, 95, 650, 300)
+    
+    line(95, 50, 300, 550)
+    
+    /*line(50, 95, 750, 500)
+    
+    line(50, 95, 650, 300)
+    
+    line(50, 95, 650, 700)*/
+    
+    ellipse(100, 200, 40, 40);
+    fill(0);
+    noStroke();
+   
+    ellipse(200, 200, 40, 40);
+    fill(0);
+    noStroke();
+    
+    ellipse(300, 200, 40, 40);
+    fill(0);
+    noStroke();
+    
+    ellipse(100, 300, 40, 40);
+    fill(0);
+    noStroke();
+   
+    ellipse(200, 300, 40, 40);
+    fill(0);
+    noStroke();
+    
+    ellipse(300, 300, 40, 40);
+    fill(0);
+    noStroke();
+    
+    ellipse(100, 100, 40, 40);
+    /*fill(152, 255, 152);*/
+    fill(255);
+    noStroke();
+   
+    ellipse(200, 100, 40, 40);
+    fill(0);
+    noStroke();
+    
+    ellipse(300, 100, 40, 40);
+    fill(255);
+    noStroke();
+    
+    
+
+}
+  
+    
