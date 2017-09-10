642ca6d51910c072d40efc644bace3da5f969437
kriziafern <kriziafernando@gmail.com>
2017-09-10 00:21:07 -0400

    update folder name and final design

--- ICM/sketch.js
+++ ICM/sketch.js
@@ -1,49 +1,52 @@
 function setup() {
-    createCanvas(400, 600);
+    createCanvas(400, 400);
     background(0, 0, 255);
 }
 
 function draw() {
  
     line(100, 100, 300, 300);
-    strokeWeight(10);  // Beastly
     
+    //5
+    strokeWeight(10);
     ellipse(100, 200, 40, 40);
-    fill(0);
-    noStroke();
-   
+    fill(255);
+    
+    //6
     ellipse(200, 200, 40, 40);
     fill(0);
-    noStroke();
     
+    //7
     ellipse(300, 200, 40, 40);
     fill(0);
-    noStroke();
-    
+
+    //8
     ellipse(100, 300, 40, 40);
     fill(0);
-    noStroke();
    
+    //9
     ellipse(200, 300, 40, 40);
-    fill(0);
-    noStroke();
-    
-    ellipse(300, 300, 40, 40);
-    fill(0);
-    noStroke();
-    
-    ellipse(100, 100, 40, 40);
-    /*fill(152, 255, 152);*/
     fill(255);
-    stroke(0)
-   
+    
+    //1
+    ellipse(300, 300, 40, 40);
+    fill(255);
+    
+    //2
+    ellipse(100, 100, 40, 40);
+    fill(0);
+
+    //3
     ellipse(200, 100, 40, 40);
     fill(0);
-    noStroke();
-    
+
+    //4
+    strokeWeight(10);
     ellipse(300, 100, 40, 40);
-    fill(255);
-    noStroke();
+    fill(0);
+    
+    /*(152, 255, 152)- MINT
+    (0, 0, 255) - BLUE */
     
 
 }
